5e1c45609e4992fc19b6b4c0594fc5bf3da8f10f
ulumuddin87 <dafiq1987@gmail.com>
2025-11-04 13:03:00 +0700

    kirim emal sungguhan

--- scrip.js
+++ scrip.js
@@ -5,19 +5,7 @@ document.getElementById("ctaBtn").addEventListener("click", () => {
   });
 });
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-  const statusMsg = document.getElementById("statusMsg");
-
-  if (!name || !email || !message) {
-    statusMsg.textContent = "Harap isi semua kolom.";
-    statusMsg.style.color = "red";
-    return;
-  }
 
   // Simulasi pengiriman
   setTimeout(() => {
